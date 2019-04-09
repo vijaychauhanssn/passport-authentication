97ce0941731ee21c1107a958ee1b844e403c9b18
vijay chauhan <vijaychauhanssn@gmail.com>
2019-04-09 12:39:58 +0530

    passport authentication

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+app.use(session({ secret: 'passport-authentication', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 if(!isProduction) {
   app.use(errorHandler());
 }
 
 //Configure Mongoose
-mongoose.connect('mongodb://localhost/passport-tutorial');
+mongoose.connect('mongodb://localhost/passport-authentication');
 mongoose.set('debug', true);
 
 //Models & routes
